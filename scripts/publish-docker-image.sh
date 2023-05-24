c8a1954ab3e588faf6dc1db00045866474e51217
Sean Story <sean.j.story@gmail.com>
2023-05-24 18:37:40 -0500

    Fix push syntax and add wait

--- scripts/publish-docker-image.sh
+++ scripts/publish-docker-image.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/docker-env.sh"
 /bin/bash "${SCRIPT_DIR}/docker-login.sh"
 
-docker push -t "$BASE_IMAGE_TAG_FINAL"
+docker push "$BASE_IMAGE_TAG_FINAL"
