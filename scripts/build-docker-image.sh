93c69499ee88c43569ca3606d347d295b0c4aa06
Sean Story <sean.j.story@gmail.com>
2023-05-24 16:02:22 -0500

    Move scripts/ to top level

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -5,6 +5,6 @@ echo "We made it to the script!"
 echo "our working dir is: $(pwd)"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-version_file="${SCRIPT_DIR}/../../VERSION"
+version_file="${SCRIPT_DIR}/../VERSION"
 version=$(head $version_file)
 echo "And the version is: ${version}"
