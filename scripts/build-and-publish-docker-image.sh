172886a0fb48353bcd30295835d6c0f39d69455d
Sean Story <sean.j.story@gmail.com>
2023-06-08 16:17:22 -0500

    Change secret

--- scripts/build-and-publish-docker-image.sh
+++ scripts/build-and-publish-docker-image.sh
@@ -8,8 +8,8 @@ set -e
 VAULT_ADDR=${VAULT_ADDR:-https://vault-ci-prod.elastic.dev}
 VAULT_USER="docker-swiftypeadmin"
 echo "Fetching Docker credentials for '$VAULT_USER' from Vault..."
-DOCKER_USER=$(vault read -address "${VAULT_ADDR}" -field login secret/ci/elastic-data-extraction-service/${VAULT_USER})
-DOCKER_PASSWORD=$(vault read -address "${VAULT_ADDR}" -field password secret/ci/elastic-data-extraction-service/${VAULT_USER})
+DOCKER_USER=$(vault read -address "${VAULT_ADDR}" -field user_20230609 secret/ci/elastic-data-extraction-service/${VAULT_USER})
+DOCKER_PASSWORD=$(vault read -address "${VAULT_ADDR}" -field secret_20230609 secret/ci/elastic-data-extraction-service/${VAULT_USER})
 
 echo "Logging into docker..."
 buildah login --username="${DOCKER_USER}" --password="${DOCKER_PASSWORD}" docker.elastic.co
