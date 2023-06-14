4f833cc11d4ac191cb3ca23951e1fe11a663f769
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-06-14 15:27:13 +0200

    Add exceptions for tika and missing EOF lines

--- nginx/lua/tika-response.lua
+++ nginx/lua/tika-response.lua
@@ -11,11 +11,41 @@ end
 if eof then
     local whole = table.concat(buffered)
     ngx.ctx.buffered = nil
+
     local cjson = require "cjson"
     local body = cjson.decode(whole)
-    -- TODO add more response data
-    local response = cjson.encode({
-        extracted_text = body[1]["X-TIKA:content"]
-    })
-    ngx.arg[1] = response
+
+    local response = {}
+
+    if not body[1] then
+        response["error"] = "Internal Server Error"
+        response["message"] = "Unexpected response format from Tika server."
+    elseif not body[1]["X-TIKA:content"] then
+
+        for k, v in pairs(body[1]) do
+            if string.find(k, "X-TIKA:EXCEPTION") then
+                local i = string.find(v, "\n")
+                local message = ""
+                if not i then
+                    message = v
+                else
+                    -- Tika errors are often massive Java stack traces.
+                    -- We can see these in full in the tikaserver.log so only send first line back.
+                    message = string.sub(v, 1, i - 1)
+                end
+                response["error"] = "Content Extraction Error"
+                response["message"] = k .. " - " .. message
+                break
+            end
+        end
+
+        if not response["error"] then
+            -- if no exceptions are returned, content was extracted but it was likely a blank document
+            response["extracted_text"] = ""
+        end
+    else
+        response["extracted_text"] = body[1]["X-TIKA:content"]
+    end
+
+    ngx.arg[1] = cjson.encode(response)
 end
