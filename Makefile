2ce5b2688ba93d40687622b4b94a85ef131542e8
Tarek ZIADE <tarek@ziade.org>
2023-06-19 10:25:00 -0700

    now doing extraction

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: test
 
+
+current_dir = $(shell pwd)
+
 bin/python:
 	python3 -m venv .
 	bin/pip install -r tests-requirements.txt
@@ -9,6 +12,6 @@ e2e: bin/python
 	- docker stop extraction-service
 	- docker rm extraction-service
 	docker build --platform=linux/arm64 -t extraction-service .
-	docker run -d -p 8090:8090 -it --name extraction-service extraction-service
+	docker run -v $(current_dir)/tests/samples:/tmp -d -p 8090:8090 -it --name extraction-service extraction-service
 	sleep 5
 	bin/python3 tests/e2e.py
--- tests/e2e.py
+++ tests/e2e.py
@@ -1,6 +1,8 @@
+from contextlib import asynccontextmanager
 import aiohttp
 import asyncio
 import logging
+import os
 
 
 ROOT = "http://localhost:8090"
@@ -8,13 +10,50 @@ logger = logging.getLogger("extraction-service")
 logging.basicConfig(level=logging.INFO)
 
 
+@asynccontextmanager
+async def logs_on_error():
+    try:
+        yield
+    except Exception:
+        log_dir = "/var/log/"
+        target_dir = "log"
+        docker_cp = "docker cp extraction-service"
+
+        os.makedirs(target_dir, exist_ok=True)
+
+        # copy over all logs files
+        for log_file in (
+            "openresty_errors.log",
+            "openresty.log",
+            "tika.log",
+            "tikaserver.log",
+        ):
+            cmd = f"docker cp extraction-service:{log_dir}/{log_file} {target_dir}/{log_file}"
+            os.system(cmd)
+
+        logger.critical(f"test failed check logs in {log_dir}")
+        raise
+
+
+@logs_on_error()
 async def main():
     # pinging root
     logger.info(f"Calling {ROOT}")
     async with aiohttp.ClientSession() as session:
         async with session.get(ROOT) as resp:
+            logger.info(await resp.text().strip())
             assert resp.status == 200
+
+    logger.info("OK")
+
+    # extracting a pdf
+    url = f"{ROOT}/extract_local_file_text/?local_file_path=sample.pdf"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.put(url, headers={"Accept": "application/json"}) as resp:
+            # XXX assert the result of the extraction
             logger.info(await resp.text())
+            assert resp.status == 200
 
     logger.info("OK")
 
