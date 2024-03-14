86d35d91644aa11c343b37e9b21edaacd63834cc
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2024-03-14 14:25:37 +0100

    Add license headers to code files

--- nginx/lua/tika-request-headers.lua
+++ nginx/lua/tika-request-headers.lua
@@ -1,3 +1,9 @@
+--[[
+Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+or more contributor license agreements. Licensed under the Elastic License 2.0;
+you may not use this file except in compliance with the Elastic License 2.0.
+]]
+
 -- use args to set headers
 local args = ngx.req.get_uri_args()
 local fetchKey = args["local_file_path"]
