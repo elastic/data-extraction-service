87894a454a9ec13ef99d3b57e47ef5a4b93514bb
Chenhui Wang <wangch079@outlook.com>
2024-10-02 14:58:31 +0800

    temporarily push to enterprise-search namespace as well

--- drivah.toml.sh
+++ drivah.toml.sh
@@ -10,9 +10,12 @@ DOCKER_NAMESPACE="integrations"
 DOCKER_PROJECT_NAME="data-extraction-service"
 DOCKER_IMAGE_BASE="${DOCKER_REPO}/${DOCKER_NAMESPACE}/${DOCKER_PROJECT_NAME}"
 
+# We will continue to build and push image to namespace enterprise-search in 8.16 and 8.17
+TEMP_DOCKER_NAMESPACE="enterprise-search"
+TEMP_DOCKER_IMAGE_BASE="${DOCKER_REPO}/${TEMP_DOCKER_NAMESPACE}/${DOCKER_PROJECT_NAME}"
 
 cat <<EOF
 [container.image]
-names = ["${DOCKER_IMAGE_BASE}"]
+names = ["${DOCKER_IMAGE_BASE}", "${TEMP_DOCKER_IMAGE_BASE}"]
 tags = ["${VERSION}"]
 EOF
